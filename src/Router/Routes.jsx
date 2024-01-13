118f6105555e387ec90decd1b45d90c44a65cc93
Mizanur Rahman <mizanurewu@gmail.com>
2024-01-13 23:00:28 +0600

    private route added

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,38 +1,46 @@
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import Bills from "../Pages/Todos/Bills/Bills";
 import Todos from "../Pages/Todos/Todos";
 import DependentSelector from "../Pages/DependentSelector/DependentSelector";
 import Xlsx from "../Pages/Xlsx/Xlsx";
+import Login from "../Pages/Login/Login";
+import PrivateRoute from "./ProtectedRoute";
 
- export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main/>,
-      children:[
-        {
-            path:"/",
-            element:<Home/>
-        },
-        {
-            path:"/todos",
-            element:<Todos/>
-        },
-        {
-            path:"/bills",
-            element:<Bills/>
-        },
-        {
-            path:"/dependentSelector",
-            element:<DependentSelector/>
-        },
-        {
-            path:"/xlsx",
-            element:<Xlsx/>
-        }
-      ]
-    },
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/todos",
+        element: <Todos />,
+      },
+      {
+        path: "/bills",
+        element: <Bills />,
+      },
+      {
+        path: "/dependentSelector",
+        element: <DependentSelector />,
+      },
+      {
+        path: "/xlsx",
+        element: (
+          <PrivateRoute>
+            <Xlsx />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/Routes.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App.jsx";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
+    <App />
     <ReactQueryDevtools buttonPosition={"bottom-left"} initialIsOpen={false} />
   </QueryClientProvider>
 );
