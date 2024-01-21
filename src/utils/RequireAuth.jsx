2b418dd585eb9a722d61e7f1213c538d01b43ec3
Mizanur Rahman <mizanurewu@gmail.com>
2024-01-21 23:58:33 +0600

    RequireAuth

---
+++ src/utils/RequireAuth.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import useLocalStorage from "./useLocalStorage";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../App";
+
+const RequireAuth = ({ pageName, children }) => {
+  const { isLoggedIn } = useContext(AuthContext);
+  const localStorage = useLocalStorage();
+  const { getAuthToken } = localStorage;
+  console.log(isLoggedIn);
+  if (!getAuthToken()) {
+    return <Navigate to="/login" />;
+  }
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default RequireAuth;
