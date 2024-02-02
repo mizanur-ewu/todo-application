dd2808a61571edf894827be6c51f5d3d89ea6df5
Mizanur Rahman <mizanurewu@gmail.com>
2024-02-02 12:53:23 +0600

    dashboard

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,35 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Pages/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import useLocalStorage from "../utils/useLocalStorage";
+export const navbarLinks = [
+  {
+    id: 1,
+    name: "Bills",
+    link: "/bills",
+  },
+  {
+    id: 2,
+    name: "Todos",
+    link: "/todos",
+  },
+  {
+    id: 3,
+    name: "Dependent Selector",
+    link: "/dependentSelector",
+  },
+  {
+    id: 4,
+    name: "XLSX",
+    link: "/xlsx",
+  },
+  {
+    id: 5,
+    name: "Settings",
+    link: "/settings",
+  },
+  {
+    id: 6,
+    name: "Login",
+    link: "/login",
+  },
+];
 const Main = () => {
-  const sideBars = [
-    {
-      id: 1,
-      name: "My Dashboard",
-    },
-    {
-      id: 2,
-      name: "My Profile",
-    },
-    {
-      id: 3,
-      name: "Change Password",
-    },
-  ];
+  const localStorage = useLocalStorage();
+  const { getAuthToken } = localStorage;
+  const userPagePermissions = getAuthToken()?.userPagePermission;
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
   return (
     <div>
-      <Navbar />
-      <div className="border h-screen bg-red-400 flex">
-        <div className="w-[200px] overflow-y-auto h- border">
+      <div
+        className={`h-screen bg-slate-200 flex ${
+          isSidebarOpen ? "ml-0" : "-ml-[200px]"
+        }`}
+      >
+        <div className="w-[200px] overflow-y-auto border ">
+          <button
+            className="absolute top-2 right-2 p-2 bg-gray-300 rounded-full"
+            onClick={toggleSidebar}
+          >
+            Toggle Sidebar
+          </button>
           <div>--SIDE BAR--</div>
-          <div>{
-            sideBars?.map((sideBar)=>(
-              <p key={sideBar.id}>{sideBar?.name}</p>
-            ))
-            }</div>
+          <div>
+            {userPagePermissions?.map(
+              (permission) =>
+                navbarLinks?.some(
+                  (link) => link?.name === permission?.name
+                ) && (
+                  <ul key={permission.name}>
+                    <li className="bg-blue-600 border" key={permission?.id}>
+                      <Link
+                        className="m-2 px-1 rounded-md text-sm"
+                        to={permission?.link}
+                      >
+                        {permission?.name}
+                      </Link>
+                    </li>
+                  </ul>
+                )
+            )}
+          </div>
         </div>
-        <div className="flex-grow  border">
+        <div className="flex-grow border">
           <Outlet />
         </div>
       </div>
